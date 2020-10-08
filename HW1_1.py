ce42008fc851d7874a993ba8fdbaeb843af0034d
momosang <59814840+momosang98@users.noreply.github.com>
2020-10-08 18:38:46 +0800

    Update HW1_1.py
    Add input & correct Aitken's technique

--- HW1_1.py
+++ HW1_1.py
@@ -10,15 +10,13 @@ def main(p0,n):
         if abs(p-p0) < 1.0e-8:
             return u'after %s times iteration，we get x = %s' % (i+1 , p)
         p0 = p
+        print('x = ',p0,',','fx = ',f(p0))
     return u' can not generate a convergent sequence '
 
-
-
 if __name__ == '__main__':
-    print('With initial guess x0=0.1',',',main(0.1,100))
-    print('With initial guess x0=0.9',',',main(0.9, 100))
-    print('With initial guess x0=0.3',',',main(0.3, 100))
-    print('With initial guess x0=-1.01',',',main(-1.01, 100))
+    x0 = float(input('Please input initial guess x0 except 1, −2, 0, −1'))
+    print('With initial guess x0 = ',x0,'\n',main(x0,10))
+
 
 #(b) check the convergence of the iteration above accelerated by Aitken’s technique
 def f(x):
@@ -26,30 +24,22 @@ def f(x):
 def phi(f,x):
     return f+x
 
-def y(f,x):
-    return phi(f,x)
-
-def z(f,y):
-    return phi(f,y)
-
 def roll(x):
-    f1=f(x)
-    y1=y(f1,x)
-    f2=f(y1)
-    z1=z(f2,y1)
-    xx=(x*z1-y1**2)/(x+z1-2*y1)
+    y1 = phi(f(x),x)
+    y2 = phi(f(y1),y1)
+    xx = (x*y2-y1**2)/(x+y2-2*y1)
     return xx
 
-
-
-def main(x,e):
-    ans = []
-    ans.append(roll(x))
-    ans.append(roll(ans[0]))
+def main(x0,e):
     for i in range(500):
-        if abs(ans[-1] - ans[-2]) < e:
-            return  i+1,ans[-1]
-        ans.append(roll(ans[-1]))
+        c = f(roll(x0))
+        x = roll(x0)
+        if abs(c) < e:
+            return u'Through Aitken’s technique,after %s times iteration，we guess x = %s, f(x) = %s' % (i+1 , x, c)
+        x0 = x
+    print('can not generate a convergent sequence' )
+
 
 if __name__ == '__main__':
-    print('Through Aitken’s technique,after %s times iteration，we guess x = %s' % (main(0.01,1e-8)))
+    x0 = float(input('Please input initial guess x0'))
+    print('With initial guess x0= ',x0,'\n',main(x0,1.0e-8))
